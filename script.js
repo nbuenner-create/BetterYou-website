75848f6c021d59272b2c75f3ffa207098ed6db5b
nbuenner-create <nbuenner@gmx.de>
2025-11-15 12:39:56 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const plans = {
+"Montag": ["Schulterdrücken", "Lateral Raises", "Front Raises", "Overhead Trizeps", "Trizeps Pushdown", "Dips", "Cable Flys", "Butterfly"],
+"Dienstag": ["Deadlifts", "Rudern", "Latziehen", "T Bar Rudern", "Reverse Butterfly", "Shrugs", "Preacher Curls", "Hammer Curls"],
+"Mittwoch": ["Beinpresse", "Beinstrecker", "Beinbeuger", "Wadenheben", "Bauch Twists", "Bauch Crunches", "Leg Raises"],
+"Donnerstag": ["Cardio", "Bauch Training", "Dehnen", "Plyometrics"],
+"Freitag": ["Preacher Curls", "Hammer Curls", "Cable Curls", "Reverse Curls", "Chest Flys", "Butterfly", "Dips"],
+"Samstag": ["Cardio", "Bauch Training", "Dehnen", "Plyometrics"],
+"Sonntag": ["Deadlifts", "Rudern", "TBar Rudern", "Latziehen", "Reverse Butterfly", "Schulterdrücken", "Lateral Raises", "Front Raises"],
+"Ernährung": ["Wasser", "Protein Shake", "Zinc", "Creatin", "Magnesium", "Ashwaganda", "kein Zucker", "Meal Prep"]
